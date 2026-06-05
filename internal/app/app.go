6138007daa617370231282680d7c1b091b361d1f
Mastermou8 <mouperalta@gmail.com>
2026-06-05 16:20:31 -0400

    Signed-off-by: Mauricio Peralta <mouperalta@gmail.com>
    added a bunch of new files and made some modifications to existing ones. The new files include a .gitignore, a docker-compose.yml, and several Go source files for handling workouts in an application. The modifications include changes to the app initialization and routing to accommodate the new workout handling functionality.

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,50 @@
 package app
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/mastermou8/goProject/internal/api"
+	"github.com/mastermou8/goProject/internal/store"
+	"github.com/mastermou8/goProject/migrations"
 )
 
+// Application struct to hold application-wide dependencies
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
+// NewApplication initializes the application and its dependencies
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
+	}
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	app := &Application{
-		Logger: logger,
-	}
+	//out stores will go here
+	workoutStore := store.NewPostgresWorkoutStore(pgDB)
 
+	// our handlers will go here
+	workoutHandler := api.NewWorkoutHandler(workoutStore)
+	app := &Application{
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
+	}
+	//returns application and nil (not significant errors)
 	return app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "OK")
 }
