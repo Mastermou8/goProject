affc9f4080f8eddfa5db94f117157c44a394b477
Mauricio Peralta <mouperalta@gmail.com>
2026-06-11 10:10:23 -0400

    feature: implemented errors in code lol

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -3,8 +3,11 @@ package middleware
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"github.com/mastermou8/goProject/internal/store"
+	"github.com/mastermou8/goProject/internal/tokens"
+	"github.com/mastermou8/goProject/internal/utils"
 )
 
 type UserMiddleware struct {
@@ -26,4 +29,33 @@ func getUser(r *http.Request) *store.User {
 		panic("user not found in context") // bad actor call
 	}
 	return user
+}
+
+func (um *UserMiddleware) Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Add("Vary", "Authorization")
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			r = setUser(r, store.AnonymousUser)
+			next.ServeHTTP(w, r) // go to next function in the chain
+			return
+		}
+		headerParts := strings.Split(authHeader, " ") //Bearer <TOKEN>
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error": "invalid authorization header"})
+			return
+		}
+		token := headerParts[1]
+		user, err := um.UserStore.GetUserToken(tokens.ScopeAuth, token)
+		if err != nil {
+			utils.WriteJSON(w, http.StatusUnauthorized, utils.Envelope{"error": "token expired or invalid"})
+
+			return
+		}
+		r = setUser(r, user)
+		next.ServeHTTP(w, r)
+
+	})
+
 }
